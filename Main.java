897ddc50c87c6f8d51bb07b0cb0add12b38b7239
Andrmd <ukai_keisuke@yse-c.net>
2017-07-06 11:42:30 +0900

    mainjavaupdate

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 public class Main{
   public static void main(String[] args) {
-    //instance
+    //instance add
     InterfaceMan im = new InterfaceMan();
     Quizman qm = new Quizman();
 
-    //Aloha_method
-    im.aloha();
-    qm.aloha();
+    //message input
+    im.showMsgInput();
+    //quizmanに回答を渡して判定
+    int userAnswer = im.getUserAnswer();
+    if(qm.judhe(userAnswer)){
+      im.showMsgAtari();
+    }else{
+      im.showMsgHazure();
+    }
   }
 }
