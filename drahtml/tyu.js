30324713af8da57233caff13c3c84583af512e64
Andrmd <ukai_keisuke@yse-c.net>
2017-07-13 11:33:02 +0900

    array updated

--- drahtml/tyu.js
+++ drahtml/tyu.js
@@ -1,7 +1,5 @@
 function Button1(){
-  kekka[0] = "test1";
-  kekka[1] = "test2";
-  kekka[2] = "test3";
+  kekka = ['test1','test2','test3'];
   var rand = Math.floor( Math.random() * 3);
   window.alert(kekka[rand]);
 }
